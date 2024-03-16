5438ba8c7a26be656bcf27ce108354b570c99ed0
Tanmay Singal <tanmaysingal@icloud.com>
2024-03-15 17:20:21 -0700

    homescreen-done

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
-import { Button } from "@/components/ui/button";
+import { ArrowRight } from 'lucide-react';
+import Link from "@/node_modules/next/link";
 
 export default function Home() {
   return (
     <main className="">
-      <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
-        <h1 className="test-5xl font-bold">
-          Welcome to CloudStash.
-          <br/>
-          Storing everything for you and your business needs. All in one place.
-        </h1>
+      <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800">
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="test-5xl font-bold">
+            Welcome to DropBox.
+            <br />
+            Storing everything for you and your business needs. All in one place.
+          </h1>
 
-        <p className="pb-20">
-        Get the storage you and your teams need with security features like file recovery, password protection, watermarking, and viewer history. Trackable links show when someone has opened a shared file and how long they’ve engaged with it. Plus, you can turn off access for any individual at any time without affecting others’ permissions. Directly edit PDFs and use video tools to streamline feedback and approval processes. Automate manual processes with tools like eSignature templates, which let you reuse documents in seconds.
-        </p>
+          <p className="pb-20">
+            Get the storage you and your teams need with security features like file recovery, password protection, watermarking, and viewer history. Trackable links show when someone has opened a shared file and how long they’ve engaged with it. Plus, you can turn off access for any individual at any time without affecting others’ permissions. Directly edit PDFs and use video tools to streamline feedback and approval processes. Automate manual processes with tools like eSignature templates, which let you reuse documents in seconds.
+          </p>
+
+          <Link href='/dashboard' className="flex cursor-pointer bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRight className='ml-10' />
+          </Link>
+        </div>
+
+        <div className='bg-[#1E1919] dark:bg-slate-800 h-full p-10'>
+          <video autoPlay loop muted className='rounded-lg'>
+            <source src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4" type="video/mp4" />
+          </video>
+        </div>
       </div>
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,15 +9,15 @@ function Header() {
     <header className='flex items-center justify-between'>
       <Link href="/" className='flex items-center space-x-2'>
         <div className="bg-[#0160FE] w.fit">
-          <Image src="https://www.shareicon.net/download/512x512/2015/09/25/646008_cloud_512x512.png" alt="logo" height={50} width={50} />
+          <Image src="https://www.shareicon.net/download/64x64/2016/06/19/603913_dropbox_512x512.png" alt="logo" height={50} width={50} />
         </div>
-        <h1 className='font-bold text-xl'>CloudStash</h1>
+        <h1 className='font-bold text-xl'>DropBox</h1>
       </Link>
       <div className='px-5 flex space-x-2 items-center'>
-        <ThemeToggler/>
+        <ThemeToggler />
         <UserButton afterSignOutUrl='/' />
         <SignedOut>
-          <SignInButton afterSignInUrl='/dashboard' mode='modal'/>
+          <SignInButton afterSignInUrl='/dashboard' mode='modal' />
         </SignedOut>
       </div>
     </header>
